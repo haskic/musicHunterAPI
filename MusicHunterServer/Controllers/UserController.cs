cd9f677acb9a775b759dac3144c035ab885672a4
Alexander Speek <kot3sany@gmail.com>
2020-09-06 20:09:09 +0300

    getTracks requestHandler added to UserController

--- MusicHunterServer/Controllers/UserController.cs
+++ MusicHunterServer/Controllers/UserController.cs
@@ -6,6 +6,8 @@ using Google.Apis.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MusicHunterServer.Data;
 using MusicHunterServer.Models;
@@ -43,6 +45,15 @@ namespace MusicHunterServer.Controllers
             _dbContext.PlaylistUserRelations.Add(playlist);
             await _dbContext.SaveChangesAsync();
             return JsonConvert.SerializeObject(new { message = "Track was added to User", status = true });
+        }
+        [Route("api/user/getTracks")]
+        [HttpGet]
+        public string GetTracksOfUser(string userHash)
+        {
+            _logger.LogInformation($"Get tracks [userHash  = {userHash}]");
+            var tracks = _dbContext.Tracks.FromSqlRaw("select T.Id,T.Artist,T.HashUrl,T.ImageUrl,T.Name,T.OwnerId from TrackUserRelations join Tracks as T on TrackHash = T.HashUrl where UserHash like @userHash", new SqlParameter("@userHash", userHash));
+             
+            return JsonConvert.SerializeObject(new { messsage = "Tracks of ", tracks = JsonConvert.SerializeObject(tracks), status = true});
         }
     }
 }
--- MusicHunterServer/Data/UserDbContext.cs
+++ MusicHunterServer/Data/UserDbContext.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MusicHunterServer.Data
 {
-    public class UserDbContext: DbContext
+    public class UserDbContext : DbContext
     {
         public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
         {
@@ -17,10 +17,11 @@ namespace MusicHunterServer.Data
         public DbSet<Conversation> Conversations { get; set; }
         public DbSet<ConversationRelation> ConversationRelations { get; set; }
         public DbSet<Participant> Participants { get; set; }
-        
+
         public DbSet<TrackToUser> TrackUserRelations { get; set; }
-        public DbSet<PlaylistToUser> PlaylistUserRelations{ get; set; }
+        public DbSet<PlaylistToUser> PlaylistUserRelations { get; set; }
 
-
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
     }
 }
